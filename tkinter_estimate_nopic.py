2bde7690d18b676c309b381c6e86f92b25738739
MohamedElbekkar <76691671+MohamedElbekkar@users.noreply.github.com>
2021-06-19 01:53:23 +0100

    Create tkinter_estimate_nopic.py

---
+++ tkinter_estimate_nopic.py
@@ -0,0 +1,32 @@
+import tkinter as tk
+import pickle
+ 
+ 
+f = open('myVariables.txt', 'rb')
+m_curr , b_curr = pickle.load(f)
+f.close()
+#print(m_curr , b_curr)
+ 
+root= tk.Tk()
+ 
+canvas1 = tk.Canvas(root, width = 400, height = 300)
+canvas1.pack()
+ 
+entry1 = tk.Entry (root) 
+canvas1.create_window(200, 140, window=entry1)
+ 
+lbl=tk.Label(root, text="Enter your mileage", fg='black', font=("Helvetica", 26))
+lbl.place(x=60, y=50)
+root.resizable(0, 0)
+
+def getEstimatePrice ():  
+    mileage = entry1.get()
+ 
+    label1 = tk.Label(root, text= m_curr*float(mileage) + b_curr)
+    canvas1.create_window(200, 230, window=label1)
+ 
+ 
+button1 = tk.Button(text='Get estimate price', command=getEstimatePrice)
+canvas1.create_window(200, 180, window=button1)
+ 
+root.mainloop()
